ed2dffcdc6548a9d79d1ca8a39f9d5053b89c4d1
Adrian <23103135@usc.edu.ph>
2025-07-15 14:50:36 +0800

    Improved layout

--- my-app/src/app/dashboard/products/[id]/page.js
+++ my-app/src/app/dashboard/products/[id]/page.js
@@ -5,7 +5,7 @@ import Products from '../../components/products'
 const Page = () => {
   return (
     <div className={styles.container}>
-        <h1 style={{color: 'black', fontSize: '26px', fontWeight: 'bold'}}>Produt List</h1>
+        <h1 style={{color: 'black', fontSize: '26px', fontWeight: 'bold', marginLeft: '50px', marginBottom: '50px'}}>Produt List</h1>
         <div className={styles.content}>
           <Products />
         </div>
